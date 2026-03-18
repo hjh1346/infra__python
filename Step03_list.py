ec5796b06882608b3fc9902444920404b35deccb
kimyujin <hjh1346@naver.com>
2026-03-18 14:32:59 +0900

    Step03_list.py 완성함

--- Step03_list.py
+++ Step03_list.py
@@ -1,1 +1,17 @@
+nums=[10, 20, 30]
 names=["김구라", "해골", "원숭이"]
+datas=[10, "xxx", True]
+nums.append(40)
+nums_len= len(nums)
+
+name0=names[0]
+
+del names[0]
+
+names.remove("원숭이")
+
+nums.pop()
+result = nums.pop()
+
+
+print("종료 합니다")
